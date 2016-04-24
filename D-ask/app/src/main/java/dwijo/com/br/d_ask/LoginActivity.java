69da66a42e4ddce938ddbaaaf5cf1e77410438a5
William França <williamucep@hotmail.com>
2016-04-24 03:03:37 -0300

    Login Starter Pack #1
    Cria o login com poucos recursos (muitos poucos mesmos!).

--- D-ask/app/src/main/java/dwijo/com/br/d_ask/LoginActivity.java
+++ D-ask/app/src/main/java/dwijo/com/br/d_ask/LoginActivity.java
@@ -3,50 +3,51 @@ package dwijo.com.br.d_ask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText editText_login, editText_senha;
+
+    Button btlogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        editText_login = (EditText) findViewById(R.id.editText_login);
+        editText_senha = (EditText) findViewById(R.id.editText_senha);
+
+        // CRIA BOTÃO E CHECA SE OS CAMPOS ESTÃO VÁZIOS AO APERTAR O BOTÃO \/
+        btlogar = (Button) findViewById(R.id.btlogar);
+        btlogar.setOnClickListener (new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick (View v) {
+                if (editText_login.getText().toString().length() == 0){
+                    AlertDialog.Builder alerta = new AlertDialog.Builder(LoginActivity.this);
+                    alerta.setTitle("Aviso !");
+                    alerta.setMessage("Insira seu login ou e-mail.");
+                    alerta.setNeutralButton("Ok", null);
+                    alerta.show();
+                } else if (editText_senha.getText().toString().length() == 0){
+                    AlertDialog.Builder alerta = new AlertDialog.Builder(LoginActivity.this);
+                    alerta.setTitle("Aviso !");
+                    alerta.setMessage("Insira sua senha.");
+                    alerta.setNeutralButton("Ok", null);
+                    alerta.show();
+                }
             }
         });
+        // CRIA BOTÃO E CHECA SE OS CAMPOS ESTÃO VÁZIOS AO APERTAR O BOTÃO /\
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
