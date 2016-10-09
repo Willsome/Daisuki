d1453be827da4055b9058a1d6e4d2a24fdb006d1
Azhiraphale <ew.guedes@yahoo.com.br>
2016-10-09 20:40:25 -0300

    método de inserir pergunta
    adicionei um método de inserir perguntas temporário pois o indicado é
    fazer com que nome e senha se auto complete

--- D-ask/app/src/main/java/dwijo/com/br/d_ask/DBController.java
+++ D-ask/app/src/main/java/dwijo/com/br/d_ask/DBController.java
@@ -12,6 +12,12 @@ import java.util.ArrayList;
 public class DBController {
     private static DBCreator dbCreator =null;
     private String nome;
+    private Integer iduser;
+    private String email;
+    private Integer phone;
+    private Integer idtag;
+    private Integer idarea;
+
 
     public DBController(Context context) {
         if (dbCreator == null){
@@ -23,9 +29,26 @@ public class DBController {
      AQUI PODEMOS CRIAR OS MÉTODOS COMO LISTAR ADICIONAR E FAZER TAL COISA
      LEMBRA DE FAZER UM DIAGRAMA DE CLASSES TBM
      * */
-    public void addUsers(String nome) {
+    public void addUsers(String nome, Integer iduser, String email,Integer phone) {
         this.nome = nome;
-        String sql = "INSERT INTO users (nome) VALUES ("+nome+")";
+        this.iduser = iduser;
+        this.email = email;
+        this.phone = phone;
+
+        String sql = "INSERT INTO USER (nome,iduser,email,phone) VALUES ("+nome+','+iduser+','+email+','+phone+")";
+        SQLiteDatabase db =dbCreator.getWritableDatabase();
+        db.execSQL(sql);
+
+
+    }
+    public void mkquestion(String nome, Integer iduser,Integer idtag, Integer idarea  ) {
+        this.nome = nome;
+        this.iduser = iduser;
+        this.idtag = idtag;
+        this.idarea = idarea;
+
+
+        String sql = "INSERT INTO QUESTIONS (nome,iduser,idtag,idarea) VALUES ("+nome+','+iduser+','+idtag+','+idarea+")";
         SQLiteDatabase db =dbCreator.getWritableDatabase();
         db.execSQL(sql);
 
